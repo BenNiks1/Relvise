b56fd6d674a0c540b72389a64d338ad990a65fa5
benniks <ikke.trodde@gmail.com>
2020-12-05 15:33:51 +0300

    intro section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Intro from './components/sections/Intro'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from "../../assets/img/logo.png";
+import { MenuSvg, SearchSvg, ShopSvg } from "./icons";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="Logo" className="header__logo" />
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__list-item">
+            <a href="/#">Home</a>
+          </li>
+          <li className="header__list-item">
+            <a href="/#">Product</a>
+          </li>
+          <li className="header__list-item">
+            <a href="/#">Pricing</a>
+          </li>
+          <li className="header__list-item">
+            <a href="/#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <SearchSvg className='header__search'/>
+      <ShopSvg className='header__shop'/>
+      <MenuSvg className='header__menu'/>
+    </header>
+  );
+};
+
+export default Header;
